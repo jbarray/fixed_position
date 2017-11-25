cafe37949335cfcf568579ff0993784ce4d91fe8
jby <1614963384@qq.com>
2017-11-25 13:59:18 +0800

    下拉滚动条时,导航菜单随之变化.

--- fixed_position.js
+++ fixed_position.js
@@ -1,5 +1,5 @@
 var button=document.getElementsByTagName('li');
-// var part=document.getElementsByClassName('part');
+ var part=document.getElementsByClassName('part');
 
 //点击右边板块时,颜色改变
 function buttonClick() {
@@ -26,3 +26,29 @@ function click(){
     }
 }
 buttonClick();
+//滚动条下拉至某个板块时,右边方块也跟着变化.
+window.onscroll=function (){
+    var screenW=document.body.clientHeight+document.body.scrollTop;
+    var partW=[];
+    for(var i=0;i<part.length;i++){
+        partW.push(part[i].offsetTop);
+    }
+    if(screenW<600){
+        changeButton(0);
+    }
+   if(screenW>=600&&screenW<1000){
+       changeButton(1);
+   }
+   if(screenW>=1500&&screenW<1650){
+       changeButton(2);
+   }
+    if(screenW>=1650){
+        changeButton(3);
+    }
+};
+function changeButton(i){
+    for (var j = 0;j< button.length;j++) {
+        button[j].className = ' ';
+        button[i].className = 'on';
+    }
+}
