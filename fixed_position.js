4d3eef5ff7567b7c955a2a9c57087f8ceb0fa3de
jby <1614963384@qq.com>
2017-12-01 09:41:34 +0800

    修复问题:单击导航栏,左侧内容变换但导航栏样式未发生改变;增强代码鲁棒性

--- fixed_position.js
+++ fixed_position.js
@@ -1,53 +1,17 @@
 var button=document.getElementsByTagName('li');
  var part=document.getElementsByClassName('part');
 
-//点击右边板块时,颜色改变
-function buttonClick() {
-    if (document.getElementsByTagName('li')) {
-        var button=document.getElementsByTagName('li');
-        for (var i = 0; i < button.length; i++) {
-            button[i].className=' ';
-            button[0].className='on';
-            button[i] .onclick=click;
-        }
-    }
-}
-function click(){
-    var event =arguments[0] || window.event;
-    var target =event.currentTarget;
-    if (document.getElementsByTagName('li')) {
-        var button=document.getElementsByTagName('li');
-        for (var j = 0; j < button.length; j++) {
-            if (button[j].className='on') {
-                button[j].className=' ';
-            }
-        }
-        target.className='on';
-    }
-}
-buttonClick();
 //滚动条下拉至某个板块时,右边方块也跟着变化.
 window.onscroll=function (){
-    var screenW=document.body.clientHeight+document.body.scrollTop;
-    var partW=[];
+    var screenH=document.body.scrollTop;
     for(var i=0;i<part.length;i++){
-        partW.push(part[i].offsetTop);
-    }
-    if(screenW<600){
-        changeButton(0);
-    }
-   if(screenW>=600&&screenW<1300){
-       changeButton(1);
-   }
-   if(screenW>=1300&&screenW<1750){
-       changeButton(2);
-   }
-    if(screenW>=1750){
-        changeButton(3);
+        if(screenH>part[i].offsetTop-140){
+            changeButton(i);
+        }
     }
 };
-function changeButton(i){
-    for (var j = 0;j< button.length;j++) {
+function changeButton(i) {
+    for (var j = 0; j < button.length; j++) {
         button[j].className = ' ';
         button[i].className = 'on';
     }
